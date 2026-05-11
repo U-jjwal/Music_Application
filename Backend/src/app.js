4ec75adb49bdc89f88b78a8da848e55cb5457096
U-jjwal <ujjwaltiwari242@gmail.com>
2026-05-12 01:27:22 +0530

    your commit message

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,20 +3,24 @@ import cookieParser from 'cookie-parser';
 import authrouter from './routes/auth.route.js';
 import musicrouter from './routes/music.routes.js';
 import cors from 'cors'
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express();
+
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
 app.use('/api/auth', authrouter);
 app.use('/api/music', musicrouter);
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     console.log("working")
     res.send("Welcome")
 })
