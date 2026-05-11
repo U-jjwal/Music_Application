04a84a4cca94d0a1d481b17b12871506e9e62dd6
U-jjwal <ujjwaltiwari242@gmail.com>
2026-05-12 01:36:58 +0530

    update project structure for improved organization

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ['http://localhost:5173','https://auramusic-two.vercel.app'],
     credentials: true
 }));
 
